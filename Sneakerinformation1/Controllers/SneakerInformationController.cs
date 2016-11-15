0d4749c6bcc5c6f05b8851628e632d9e54ea0210
Terry McDuffie <fadess85@gmail.com>
2016-11-15 10:46:24 -0500

    Added Sneaker Information Detail View

--- Sneakerinformation1/Controllers/SneakerInformationController.cs
+++ Sneakerinformation1/Controllers/SneakerInformationController.cs
@@ -10,12 +10,7 @@ namespace Sneakerinformation1.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the Sneaker Information controller!");
+            return View();
         }
     }
 }
