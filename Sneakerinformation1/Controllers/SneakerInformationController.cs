9164aac85bc5e92da866403a52106c9ed4f1155b
Terry McDuffie <fadess85@gmail.com>
2016-11-15 09:35:04 -0500

    Added Sneaker Information Controller

---
+++ Sneakerinformation1/Controllers/SneakerInformationController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sneakerinformation1.Controllers
+{
+    public class SneakerInformationController : Controller
+    {
+    }
+}
