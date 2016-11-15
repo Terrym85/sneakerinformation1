2e34736a3ffd9cc04b09bac3e007fab91d7acd71
Terry McDuffie <fadess85@gmail.com>
2016-11-15 13:13:06 -0500

    Updated Sneaker Information Detail view

--- Sneakerinformation1/Controllers/SneakerInformationController.cs
+++ Sneakerinformation1/Controllers/SneakerInformationController.cs
@@ -10,6 +10,18 @@ namespace Sneakerinformation1.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.ShoeModel = "History Of Air Jordan";
+            ViewBag.ModelNumber = 1;
+            ViewBag.Description = "<p>On October 18, 1984, the NBA banned Michael Jordan’s black and red Air Jordan 1s for violating the league’s uniform codes. This decision triggered a craze that foreshadowed a legend in the making. </p>";
+            ViewBag.Brands = new string[]
+            {
+                "Designer: Peter Moore ",
+                "Brand Name: Nike ",
+                "Designed for: Michael Jordan",
+                "Retail: $160",
+                "Release Year: 1985"
+            };
+        
             return View();
         }
     }
