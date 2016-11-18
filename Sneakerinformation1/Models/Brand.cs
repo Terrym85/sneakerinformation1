51b4022c374dad4943b119254de5c19e52cc7781
Terry McDuffie <fadess85@gmail.com>
2016-11-17 22:20:03 -0500

    Added sneakers and brand models

---
+++ Sneakerinformation1/Models/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sneakerinformation1.Models
+{
+    public class Brand
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ Sneakerinformation1/Models/Sneakers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sneakerinformation1.Models
+{
+    public class Sneakers
+    {
+        public int Id { get; set; }
+        public string ShoeModel { get; set; }
+        public int ModelNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Brand[] Brands { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
